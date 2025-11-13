555c3d7a12eb580a125bc9ae0018170f9b8a991f
alsmawi2007 <102717194+alsmawi2007@users.noreply.github.com>
2025-11-13 15:20:01 +0300

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  SUPABASE_URL: "https://qrivkugygvycseqjfgri.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyaXZrdWd5Z3Z5Y3NlcWpmZ3JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwMjUyNzQsImV4cCI6MjA3ODYwMTI3NH0.hDjI1tim-Qoj2r-DfFV93fcfWV7nsUOo09fBdI0qnWY"
 };
